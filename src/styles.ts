07a20fc1171817b7ec15c89effc21d92b8f97708
zkd <matheus.reichemback.stang@gmail.com>
2024-10-04 14:50:43 -0300

    feat: completo o design do cabeçalho

---
+++ src/styles.ts
@@ -0,0 +1,30 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const colors = {
+  white: '#eeeeee',
+  black: '#111',
+  gray: '#333',
+  green: '#10ac84'
+}
+
+export const GlobalCss = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+    list-style: none;
+  }
+
+  body{
+    background-color: ${colors.black};
+    color: ${colors.white};
+    padding-top: 40px;
+  }
+`
+
+export const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+`
