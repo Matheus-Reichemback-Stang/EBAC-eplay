3af47a36ed617c32ecb0c8ea82c505fcb5d7e5b7
zkd <matheus.reichemback.stang@gmail.com>
2024-10-11 19:57:41 -0300

    feat: criado o cabeçalho responsivo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,19 +10,26 @@ import * as S from './styles'
 import logo from '../../assets/images/logo.svg'
 import carrinho from '../../assets/images/carrinho.svg'
 import { RootReducer } from '../../store'
+import { useState } from 'react'
 
 const Header = () => {
   const { items } = useSelector((state: RootReducer) => state.cart)
   const dispatch = useDispatch()
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   const openCart = () => {
     dispatch(open())
   }
 
   return (
-    <>
-      <S.HeaderBar>
+    <S.HeaderBar>
+      <S.HeaderRow>
         <div>
+          <S.Hamburguer onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <span />
+            <span />
+            <span />
+          </S.Hamburguer>
           <Link to="/">
             <img src={logo} alt="EPLAY" />
           </Link>
@@ -41,11 +48,25 @@ const Header = () => {
           </nav>
         </div>
         <S.CartButton onClick={openCart}>
-          {items.length} - produto(s)
+          {items.length}
+          <span> - produto(s)</span>
           <img src={carrinho} alt="Carrinho" />
         </S.CartButton>
-      </S.HeaderBar>
-    </>
+      </S.HeaderRow>
+      <S.NavMobile className={isMenuOpen ? 'is-open' : ''}>
+        <S.Links>
+          <S.LinkItem>
+            <Link to="/categories">Categorias</Link>
+          </S.LinkItem>
+          <S.LinkItem>
+            <a href="#">Novidades</a>
+          </S.LinkItem>
+          <S.LinkItem>
+            <a href="#">Promoções</a>
+          </S.LinkItem>
+        </S.Links>
+      </S.NavMobile>
+    </S.HeaderBar>
   )
 }
 
